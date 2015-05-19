cce7228a6ea654da1fdc64ffaa8f4a631b6a6b40
jnopnop <jnopnop@users.noreply.github.com>
2015-05-19 23:52:51 +0300

    ymaps +

--- web/js/main.js
+++ web/js/main.js
@@ -1,26 +1,19 @@
 (function($) {
-    
+
 }(jQuery));
 
 var myMap;
-
-// Дождёмся загрузки API и готовности DOM.
 ymaps.ready(init);
-
 function init () {
-    // Создание экземпляра карты и его привязка к контейнеру с
-    // заданным id ("map").
     myMap = new ymaps.Map('map', {
-        // При инициализации карты обязательно нужно указать
-        // её центр и коэффициент масштабирования.
-        center: [44.933212, 34.043436], // Москва
+        center: [44.933212, 34.043436],
         zoom: 17,
         type: 'yandex#satellite'
     });
 
     myMap.geoObjects
         .add(new ymaps.Placemark([44.933333, 34.043918], {
-            balloonContent: 'цвет <strong>воды пляжа бонди</strong>'
+            balloonContent: 'Airsoft Арена Крым'
         }, {
             preset: 'islands#icon',
             iconColor: '#0095b6'
